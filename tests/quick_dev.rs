75d5b660081c02196def0f1e5c479eb5e3945787
Mark Nichols <mark@zanshin.net>
2024-07-26 22:52:54 -0500

    quick_dev tests added for create, list, and delete CRUD

--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -8,10 +8,10 @@ async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
     // named parameter
-    hc.do_get("/hello?name=Mark").await?.print().await?;
+    // hc.do_get("/hello?name=Mark").await?.print().await?;
 
     // positional parameter
-    hc.do_get("/hello2/Mark").await?.print().await?;
+    // hc.do_get("/hello2/Mark").await?.print().await?;
 
     // api/login
     let req_login = hc.do_post(
@@ -22,6 +22,21 @@ async fn quick_dev() -> Result<()> {
         }),
     );
     req_login.await?.print().await?;
+
+    // create a ticket
+    let req_create_ticket = hc.do_post(
+        "/api/tickets",
+        json!({
+            "title": "Ticket AAA"
+        }),
+    );
+    req_create_ticket.await?.print().await?;
+
+    // delete a ticket
+    hc.do_delete("/api/tickets/1").await?.print().await?;
+
+    // list the tickets
+    hc.do_get("/api/tickets").await?.print().await?;
 
     // static fallback test
     // hc.do_get("/src/main.rs").await?.print().await?;

db861271181e0073b031f95ef78abf6ba853096d
Mark Nichols <mark@zanshin.net>
2024-07-29 20:28:07 -0500

    Context added along with extractor to use cookies for user id

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use serde::Deserialize;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
 
+mod ctx;
 mod error;
 mod model;
 mod web;
--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,32 +1,58 @@
 //! src/web/mw_auth.rs
 
+use async_trait::async_trait;
+use axum::extract::FromRequestParts;
+use axum::http::request::Parts;
 use axum::http::Request;
 use axum::middleware::Next;
 use axum::response::Response;
+use axum::RequestPartsExt;
 use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
 
+use crate::ctx::Ctx;
 use crate::web::AUTH_TOKEN;
 use crate::{Error, Result};
 
 pub async fn mw_require_auth<B>(
-    cookies: Cookies,
+    // cookies: Cookies,
+    ctx: Result<Ctx>,
     req: Request<B>, 
     next: Next<B>,
 ) -> Result<Response> {
     println!("->> {:12} - mw_require_auth", "MIDDLEWARE");
 
-    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
-
-    // Parse token.
-    let (user_id, exp, sign) = auth_token
-        .ok_or(Error::AuthFailNoAuthTokenCookie)
-        .and_then(parse_token)?;
-
-    // TODO: Token component validation.
+    ctx?;
 
     Ok(next.run(req).await)
 }
+
+// region:    --- Ctx Extractor {{{
+
+#[async_trait]
+impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+    type Rejection = Error;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+        println!("->> {:12} - Ctx", "EXTRACTOR");
+
+        // Use the cookies extractor
+        let cookies = parts.extract::<Cookies>().await.unwrap();
+
+        let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+
+        // Parse token.
+        let (user_id, exp, sign) = auth_token
+            .ok_or(Error::AuthFailNoAuthTokenCookie)
+            .and_then(parse_token)?;
+
+        // TODO: Token component validation.
+        Ok(Ctx::new(user_id))
+    }
+
+}
+
+// endregion: --- Ctx Extractor }}}
 
 /// Parse a token of format `user-[user-id].[expiration].[signature]`
 /// Returns (user_id, expiration, signature)
